27fb66ad0b2062c205fe64e637b98cd48df69d0e
TLMOS <pokroy-tema@yandex.ru>
2023-06-02 14:02:20 +0300

    small fix

--- services/encoder/app/app.py
+++ services/encoder/app/app.py
@@ -5,6 +5,7 @@ import pika
 from pika.adapters.blocking_connection import BlockingChannel
 from pika.spec import Basic
 from redis_om import Migrator
+import numpy as np
 import torch
 import clip
 
@@ -26,15 +27,15 @@ def on_message(channel: BlockingChannel, method: Basic.Deliver,
     timestamp = float(properties.headers['timestamp'])
 
     image = Image.open(BytesIO(body))
+    image = preprocess(image).unsqueeze(0).to('cuda')
     with torch.no_grad():
-        image = preprocess(image).unsqueeze(0).to('cuda')
         image_features = model.encode_image(image).cpu().numpy()[0]
 
     embedding = Embedding(
         sm_name=sm_name,
         source_id=source_id,
         timestamp=timestamp,
-        features=image_features.tobytes().hex()
+        features=image_features.astype(np.float32).tobytes().hex()
     )
     embedding.save()
     Embedding.db().expire(embedding.key(), settings.encoder.embedding_ttl)
